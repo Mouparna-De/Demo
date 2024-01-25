525c9ecd13aab64f830596c8603284a9ff2de184
Mouparna-De <143414214+Mouparna-De@users.noreply.github.com>
2024-01-25 06:32:35 +0000

    Initial commit

---
+++ demo1/src/App.js
@@ -0,0 +1,16 @@
+
+import Home from './Components/Home';
+import Modal1 from './Components/Model1';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Home></Home>
+      <Modal1></Modal1>
+    </div>
+  );
+}
+
+export default App;
